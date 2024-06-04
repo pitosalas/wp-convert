c4f3710722e05d9f6438208486692ed6db88bc56
Pito Salas <pitosalas@users.noreply.github.com>
2024-06-04 07:31:26 -0400

    update

--- masto_post.py
+++ masto_post.py
@@ -1,37 +1,44 @@
 import json
+from typing import Union
 from blogbuild_v2 import fix_drop_date, fix_drop_tags
 import requests
 import re
 import os
+from typing import Union
 
 MASTO_URL_FILE = "data/masto_url_list.json"
 SAFE_MODE = False
 MASTO_MAX_POST_PER_RUN = 3
 
+JsonValue = Union[str, dict[str, str]]
+
 class MastoPost:
 
     def __init__(self):
-        self.sec_token = os.getenv("MASTO_TOKEN")
-        self.masto_urls : list[str] = []
-        self.headers = {
+        self.sec_token: str = os.getenv("MASTO_TOKEN", "")
+        if self.sec_token is "":
+            raise ValueError("MASTO_TOKEN environment variable is not set")
+
+        self.masto_urls: list[str] = []
+        self.headers: dict[str, str] = {
             'Accept': 'application/json',
             'User-Agent': 'Safari',
             'Authorization': self.sec_token        
         }
-        self.masto_post_count = 0
+        self.masto_post_count: int = 0
 
     def retrieve_api_drops_from_file(self) -> None:
         with open('data/api_drops.json') as json_file:
-            self.drops = json.load(json_file)
+            self.drops: dict = json.load(json_file)
 
     def load_masto_url_file(self) -> None:
         if not os.path.exists(MASTO_URL_FILE):
             with open(MASTO_URL_FILE, "w") as json_file:
                 json.dump({}, json_file)
-                self.masto_urls = []
+                self.masto_urls: list[str] = []
         with open(MASTO_URL_FILE) as json_file:
-            loaded_file = json.load(json_file)
-            self.masto_urls = loaded_file.get("urls", [])
+            loaded_file: dict = json.load(json_file)
+            self.masto_urls: list[str] = loaded_file.get("urls", [])
             
     def save_masto_url_file(self):
         with open(MASTO_URL_FILE, "w") as json_file:
@@ -66,12 +73,12 @@ class MastoPost:
             count += 1
         print(f"""Total: {count} Drop Posts Generated""")
 
-    def create_masto_post(self, title, content, date, tags_str, url, cover):
+    def create_masto_post(self, title: str, content: str, date: str, tags_str: str, url: str, cover: str) -> None:
         if url in self.masto_urls or self.masto_post_count >= MASTO_MAX_POST_PER_RUN:
             return
-        rest_url = "https://ruby.social/api/v1/statuses"
-        status = f"""{content} {tags_str}: "{title}"({url})"""
-        json_data_dict = { "status" : status, "links" : url }
+        rest_url: str = "https://ruby.social/api/v1/statuses"
+        status: str = f"""{content} {tags_str}: "{title}"({url})"""
+        json_data_dict: dict[str, JsonValue] = {"status": status, "links": url}
         if SAFE_MODE:
             print(f"fake posting {json_data_dict}")
             self.masto_urls.append(url)
@@ -80,6 +87,8 @@ class MastoPost:
             if response.status_code == 200:
                 self.masto_urls.append(url)
                 self.masto_post_count += 1
+                print(f"successful posting {json_data_dict}")
+
 
     def run(self):
         self.retrieve_api_drops_from_file()
