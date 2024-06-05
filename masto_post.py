1ec58263959cfb8a9eef459a1333863f33c6b56a
Pito Salas <pitosalas@users.noreply.github.com>
2024-06-04 21:02:21 -0400

    update

--- masto_post.py
+++ masto_post.py
@@ -84,7 +84,7 @@ class MastoPost:
         if url in self.masto_urls or self.masto_post_count >= MASTO_MAX_POST_PER_RUN:
             return
         rest_url: str = "https://ruby.social/api/v1/statuses"
-        status: str = f"""{content} {tags_str}: "{title}"({url})"""
+        status: str = f"""{content} {tags_str}: "{title}"({rest_url})"""
         url_with_slug = self.get_slug(title, date)
         json_data_dict: dict[str, JsonValue] = {"status": status, "links": url_with_slug}
         if SAFE_MODE:
